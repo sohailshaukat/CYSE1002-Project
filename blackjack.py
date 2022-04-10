35fcbcf522c1e7d58d232bf69027bc4b2948dbce
Sohail Shaukat Selod <sohail47k@gmail.com>
2022-04-10 03:05:37 -0400

    Few additions

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,9 @@
 import random
+import re
 from deck import Deck, Card
 
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
-            'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
 
 class Hand():
@@ -23,6 +24,12 @@ class Hand():
             self.value -= 10
             self.aces -= 1
 
+    def __repr__(self):
+        deck_contains = ''
+        for card in self.cards:
+            deck_contains += '\n'+card.__str__()
+        return "Cards are " + deck_contains
+
 
 class Chips():
 
@@ -40,34 +47,68 @@ class Chips():
 def take_bet(chips):
     while True:
         try:
-            chips.bet = int(input('[+]Place your bet (Your bet should not exceed your balance): '))
+            chips.bet = int(
+                input('[+]Place your bet (Your bet should not exceed your balance): '))
         except ValueError:
             print("[-]Sorry, it has to be an integer value!")
         else:
             print("[+]Placing bet...")
             if chips.bet > chips.balance:
                 print(
-                    "[-]Insufficient Balance.. Your Current balance is {chip.balance}...")
+                    f"[-]Insufficient Balance.. Your Current balance is {chips.balance}...")
             else:
                 break
 
+
+def read_player_choice():
+    player_choice = input(
+        "[+]Enter your choice: (H) Hit / (S) Stay / (D) Double Down")
+    ch = player_choice.lower()
+    if ch in ['s', 'stay']:
+        ch == 0
+    elif ch in ['h', 'hit']:
+        ch == 1
+    elif re.search("d(ouble[\s-]*(down)?)?", ch):
+        ch == 2
+    else:
+        print("[-]Invalid Choice please try again")
+        ch = read_player_choice()
+    return ch
+
+
+def exit_event(player, event):
+    event_message = {
+        'blackjack': "has hit blackjack!",
+        'bust': ""
+    }
+    print(f"[+]")
+
+
 if __name__ == "__main__":
 
-    while True:
+    game = True
+    while game:
         print('Welcome to BlackJack!! Closest to 21 without going over 21 \nDealers hit until she reaches 17. Aces count as 1 or 11.')
-        deck=Deck()
+        deck = Deck()
         print('[+]Shuffling the deck...')
         deck.shuffle()
-
-        player_hand=Hand()
+        player_hand = Hand()
         player_hand.add_card(deck.deal())
         player_hand.add_card(deck.deal())
 
-        dealer_hand=Hand()
-        dealer_hand.add_card(deck.deal())
-        dealer_hand.add_card(deck.deal())
+        dealer_hand = Hand()
+        while dealer_hand.value < 17:
+            dealer_hand.add_card(deck.deal())
 
-        player_chips=Chips()
+        player_chips = Chips()
         print(f"[+]You've got {player_chips.balance} chips")
 
         take_bet(player_chips)
+
+        print(
+            f"[+]Player has {player_hand.__repr__} \nAnd total value is {player_hand.value}")
+
+        if player_hand.value == 21:
+
+
+        game = False
